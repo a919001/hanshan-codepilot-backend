d313979bf63f5587ea962c76672abbc9781b171d
hanshan <1470397437@qq.com>
2024-11-18 16:36:33 +0800

    初始化项目模板

---
+++ sql/create_table.sql
@@ -0,0 +1,59 @@
+# 数据库初始化
+
+-- 创建库
+create database if not exists code_pilot;
+
+-- 切换库
+use code_pilot;
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    user_account  varchar(256)                           not null comment '账号',
+    user_password varchar(512)                           not null comment '密码',
+    union_id      varchar(256)                           null comment '微信开放平台id',
+    mp_open_id     varchar(256)                           null comment '公众号openId',
+    user_name     varchar(256)                           null comment '用户昵称',
+    user_avatar   varchar(1024)                          null comment '用户头像',
+    user_profile  varchar(512)                           null comment '用户简介',
+    user_role     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    edit_time     datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint      default 0                 not null comment '是否删除',
+    index idx_unionId (union_id)
+) comment '用户' collate = utf8mb4_unicode_ci;
+
+-- 题库表
+create table if not exists question_bank
+(
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(256)                       null comment '标题',
+    description text                               null comment '描述',
+    picture     varchar(2048)                      null comment '图片',
+    user_id      bigint                             not null comment '创建用户 id',
+    edit_time    datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete    tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title)
+) comment '题库' collate = utf8mb4_unicode_ci;
+
+-- 题目表
+create table if not exists question
+(
+    id         bigint auto_increment comment 'id' primary key,
+    title      varchar(256)                       null comment '标题',
+    content    text                               null comment '内容',
+    tags       varchar(1024)                      null comment '标签列表（json 数组）',
+    answer     text                               null comment '推荐答案',
+    user_id     bigint                             not null comment '创建用户 id',
+    edit_time   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete   tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title),
+    index idx_userId (user_id)
+) comment '题目' collate = utf8mb4_unicode_ci;
+
---
+++ src/main/java/com/hanshan/codepilot/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.hanshan.codepilot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hanshan.codepilot.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
